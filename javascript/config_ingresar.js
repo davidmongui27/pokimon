a5c4e6e5d5a36942d58a411a9f93279c88422277
unknown <1234321>
2025-03-25 22:08:58 +0100

    inicio mundo pokimon

---
+++ javascript/config_ingresar.js
@@ -0,0 +1,11 @@
+function Loguear() {
+    let username = document.getElementById('username').value
+    let password = document.getElementById('password').value
+
+    if(username == 'DAVID2007' && password == 'AYUDA'){
+        alert('Usuario valido')
+    }else{
+        alert('usuario incorrecto')
+    }  
+
+}
